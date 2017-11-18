b8a0dd4cd85e08a96153be3f29969570766bba1e
Yuzhe Chen <yzxchen@gmail.com>
2017-11-18 11:23:33 -0800

    remove file

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    FB_PAGE_TOKEN: 'EAACzfFHeOCIBAHf0ziKbxSFRPnNj02nh20ZCmqakWGfE7jl6JDgGHZBO9TwoHqm5hLADjSeNuQCOpK60RavYvJbH8AlOy4jGL5rO1ADZConManj3OnrljEPnrZAH4ebYWXDd7I23kuUIsiwZAKZBh7zaAkkpxRZANO2fwvJJJ5TlgZDZD',
-    FB_VERIFY_TOKEN: 'BHYCtechmeetupbot',
-    API_AI_CLIENT_ACCESS_TOKEN: '615fc51062524a638d1b5d5fc2fe3786',
-    FB_APP_SECRET: '2c5f89063466e25d99fc4b32ebe2a7b9',
-    SERVER_URL: "https://sheltertechbot.herokuapp.com/",
-};
